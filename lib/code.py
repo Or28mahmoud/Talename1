2716df1323d6456485d6e433b946e592f8c172cd
Or28mahmoud <mahmoud.oy22@outlook.com>
2022-06-23 13:56:10 +0200

    ich lerne jz wie man mit github umgehen

--- lib/code.py
+++ lib/code.py
@@ -4,3 +4,17 @@ import time
 print("hallo mahmoud")
 time1=time.time
 print("hallo mahmoud ")
+
+
+
+from tkinter import *
+
+fenster = Tk()
+
+variable = StringVar(fenster)
+variable.set("one") # default value
+
+w = OptionMenu(fenster, variable, "one", "two", "three")
+w.pack()
+
+fenster.mainloop()
